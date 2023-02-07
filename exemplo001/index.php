be656dbebc39f2d08ed3cfd41538ae7411256fd9
Kléber Albino Moreira <kleber0a0m@gmail.com>
2023-02-07 09:05:23 -0300

    adição comparação de 2 valores

--- exemplo001/index.php
+++ exemplo001/index.php
@@ -8,9 +8,23 @@
 <body>
     <?php
         $valor = 1;
-        echo "Valoraa: $valor <br>";
+        echo "Valor: $valor <br>";
         echo 'Valor: $valor <br>';
         echo 'Valor: '.$valor.'<br>';
+
+        $n1 = 5;
+        $n2 = 4;
+
+        $n1 = (--$n1) + $n2;
+
+        echo $n1;
+
+        echo '<br>----------------------<br>';
+        $k1 = 4;
+        $k2 = 8;
+        $a = (($k1==$k2) || ($k1 + $k2 == 12));
+        echo $a;
+
     ?>
 </body>
 </html>
