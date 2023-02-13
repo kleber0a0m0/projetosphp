57123b58a0ab100b47c78067aecdfb4940d8bbca
Kléber Albino Moreira <kleber0a0m@gmail.com>
2023-02-13 10:48:03 -0300

    Tela de login

---
+++ 2023-02-13_RaizQuadrada/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Raíz Quadrada</title>
+</head>
+<body>
+    <div id="tformulario">
+        <h1>Calcular Raíz Quadrada</h1>
+        <form action="saida.php" method="get">
+            <label>
+                Digite um valor:
+            </label>
+            <input type="number" required="required" name="num" size="40"/>
+            <input type="submit" value="Calcular Raiz" class="botao"/>
+        </form>
+    </div>
+</body>
+</html>
